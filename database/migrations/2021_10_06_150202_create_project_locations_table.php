d69e44c78e03a48e6bd3df6010577f9de5c2270e
Admin <Admin>
2021-10-06 15:49:11 +0700

    migrasi tables

---
+++ database/migrations/2021_10_06_150202_create_project_locations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_locations', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('id_project');
+            $table->string('location');
+            $table->integer('id_prov');
+            $table->integer('id_district');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_locations');
+    }
+}
