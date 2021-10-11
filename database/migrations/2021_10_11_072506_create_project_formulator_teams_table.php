a2f49181e0004af6f3f58294f4278f1dbee4e060
Admin <Admin>
2021-10-11 07:26:54 +0700

    create table project_formulator_teams

---
+++ database/migrations/2021_10_11_072506_create_project_formulator_teams_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectFormulatorTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_formulator_teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_team');
+            $table->string('name')->nullable();
+            $table->integer('status')->nullable();
+            $table->string('id_applicant')->nullable();
+            $table->timestamp('date_input')->nullable();
+            $table->string('evidence_letter')->nullable();
+            $table->string('id_formulator')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_formulator_teams');
+    }
+}
