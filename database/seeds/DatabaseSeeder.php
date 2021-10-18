e6d2e08e4766b12ef687577e62e99fbc5e237089
M Zhuhriansyah Rahman <izhur@zamrudtechnology.co.id>
2021-10-18 10:52:08 +0700

    fix acl import

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,6 +4,7 @@ use App\Laravue\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Laravue\Models\Role;
+use App\Laravue\Acl;
 
 class DatabaseSeeder extends Seeder
 {
