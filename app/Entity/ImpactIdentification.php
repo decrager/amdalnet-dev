9222b8bdd68db5ba488f4c80391fafd7a828fa6e
Achmad Giovani <achmad.giovani@imaniprima.com>
2021-11-03 14:13:27 +0700

    add fillable

--- app/Entity/ImpactIdentification.php
+++ app/Entity/ImpactIdentification.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ImpactIdentification extends Model
 {
     protected $table = 'impact_identifications';
+    protected $fillable = [
+        'id_project',
+        'id_component_type',
+        'id_rona_awal',
+    ];
 }
