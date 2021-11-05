d2ab6b3ed753386d0f9b6ccde3e1fe97761d814d
Hernowo <eno.erno@gmail.com>
2021-11-06 00:28:36 +0700

    update toggle function

--- resources/js/views/home/components/TabsAnnouncement.vue
+++ resources/js/views/home/components/TabsAnnouncement.vue
@@ -1,53 +1,57 @@
 <template>
-  <div class="tabset">
-    <el-tabs type="card">
-      <el-tab-pane label="AMDAL">
-        <div v-for="amdal in amdals.data" :key="amdal.id" class="announce__box__wrapper">
-          <div class="announce__box__icon">
-            <img alt="" src="/images/list.svg">
+  <div>
+    <div v-if="showTabs" class="tabset">
+      <el-tabs type="card">
+        <el-tab-pane label="AMDAL">
+          <div v-for="amdal in amdals.data" :key="amdal.id" class="announce__box__wrapper">
+            <div class="announce__box__icon">
+              <img alt="" src="/images/list.svg">
+            </div>
+            <div class="announce__box__desc">
+              <p class="announce__box__desc__content">{{ amdal.project_type }}</p>
+              <p class="announce__box__desc__content text__special">{{ amdal.pic_name }}</p>
+              <p class="announce__box__desc__content">Dampak Potensial : {{ amdal.potential_impact }}</p>
+              <p class="announce__box__desc__content">{{ formatDate(amdal.start_date) }} | {{ amdal.feedbacks_count }}
+                Tanggapan</p>
+            </div>
+            <div class="announce__box__button">
+              <button class="button__tanggapan" @click="openDetails(amdal.id,'AMDAL')"><i class="el-icon-document" /> Berikan
+                Tanggapan
+              </button>
+            </div>
           </div>
-          <div class="announce__box__desc">
-            <p class="announce__box__desc__content">{{ amdal.project_type }}</p>
-            <p class="announce__box__desc__content text__special">{{ amdal.pic_name }}</p>
-            <p class="announce__box__desc__content">Dampak Potensial : {{ amdal.potential_impact }}</p>
-            <p class="announce__box__desc__content">{{ formatDate(amdal.start_date) }} | {{ amdal.feedbacks_count }}
-              Tanggapan</p>
+        </el-tab-pane>
+        <el-tab-pane label="UKL - UPL">
+          <div v-for="uklupl in uklupls.data" :key="uklupl.id" class="announce__box__wrapper">
+            <div class="announce__box__icon">
+              <img alt="" src="/images/list.svg">
+            </div>
+            <div class="announce__box__desc">
+              <p class="announce__box__desc__content">{{ uklupl.project_type }}</p>
+              <p class="announce__box__desc__content text__special">{{ uklupl.pic_name }}</p>
+              <p class="announce__box__desc__content">Dampak Potensial : {{ uklupl.potential_impact }}</p>
+              <p class="announce__box__desc__content">{{ formatDate(uklupl.start_date) }} | {{ uklupl.feedbacks_count }}
+                Tanggapan</p>
+            </div>
+            <div class="announce__box__button">
+              <button class="button__tanggapan" @click="openDetails(uklupl.id,'UKL')"><i class="el-icon-document" /> Berikan
+                Tanggapan
+              </button>
+            </div>
           </div>
-          <div class="announce__box__button">
-            <button class="button__tanggapan" @click="openDetails(amdal.id)"><i class="el-icon-document" /> Berikan
-              Tanggapan
-            </button>
-          </div>
-        </div>
-      </el-tab-pane>
-
-      <el-tab-pane label="UKL - UPL">
-        <div v-for="uklupl in uklupls.data" :key="uklupl.id" class="announce__box__wrapper">
-          <div class="announce__box__icon">
-            <img alt="" src="/images/list.svg">
-          </div>
-          <div class="announce__box__desc">
-            <p class="announce__box__desc__content">{{ uklupl.project_type }}</p>
-            <p class="announce__box__desc__content text__special">{{ uklupl.pic_name }}</p>
-            <p class="announce__box__desc__content">Dampak Potensial : {{ uklupl.potential_impact }}</p>
-            <p class="announce__box__desc__content">{{ formatDate(uklupl.start_date) }} | {{ uklupl.feedbacks_count }}
-              Tanggapan</p>
-          </div>
-          <div class="announce__box__button">
-            <button class="button__tanggapan" @click="openDetails(amdal.id)"><i class="el-icon-document" /> Berikan
-              Tanggapan
-            </button>
-          </div>
-        </div>
-
-      </el-tab-pane>
-
-    </el-tabs>
-
-    <AnnouncementDetail
+        </el-tab-pane>
+      </el-tabs>
+      <!-- <AnnouncementDetail
+        :announcement-id="selectedId"
+        :details="selectedAnnouncement"
+        :show="showDetailsDialog"
+      /> -->
+    </div>
+    <Details
+      :show-details="showDetails"
       :announcement-id="selectedId"
-      :details="selectedAnnouncement"
-      :show="showDetailsDialog"
+      :selected-announcement="selectedAnnouncement"
+      @handleCancelComponent="handleCancelComponent"
     />
   </div>
 </template>
@@ -56,13 +60,13 @@
 import axios from 'axios';
 import dayjs from 'dayjs';
 import 'dayjs/locale/id';
-import AnnouncementDetail from './AnnouncementDetail';
+import Details from './Details';
 import { mapGetters } from 'vuex';
 
 export default {
   name: 'AnnouncementTabs',
   components: {
-    AnnouncementDetail,
+    Details,
   },
   data() {
     return {
@@ -71,6 +75,8 @@ export default {
       showDetailsDialog: false,
       selectedId: 0,
       showInitPopup: false,
+      showDetails: false,
+      showTabs: true,
     };
   },
   computed: {
@@ -87,17 +93,20 @@ export default {
         return dayjs(String(value)).format('DD MMMM YYYY');
       }
     },
-    openDetails(id) {
+    openDetails(id, param) {
+      this.showDetails = true;
+      this.showTabs = false;
       this.selectedId = id;
       this.selectedAnnouncement = {};
       this.showDetailsDialog = true;
       axios.get('/api/announcements/' + this.selectedId)
         .then(response => {
-          this.selectedAnnouncement = response.data;
+          this.selectedAnnouncement = response.data.project;
         });
     },
-    handleCloseDialog() {
-      this.showIdDialog = false;
+    handleCancelComponent(){
+      this.showDetails = false;
+      this.showTabs = true;
     },
   },
 };
