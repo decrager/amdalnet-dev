5ad2625ddb0a56d1cd2727a844656255ad95668d
Salman Said Ramadhan <42852021+salmansaidr@users.noreply.github.com>
2021-11-05 23:27:17 +0700

    comment formulatorRoutes

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -29,7 +29,7 @@ import projectRoutes from './modules/project';
 // import paramRoutes from './modules/params';
 import uklUplRoutes from './modules/uklupl';
 import initiatorRoutes from './modules/initiator';
-import formulatorRoutes from './modules/formulator';
+// import formulatorRoutes from './modules/formulator';
 import lukRoutes from './modules/luk';
 import tukRoutes from './modules/tuk';
 import expertBankRoutes from './modules/expert-bank';
@@ -209,7 +209,7 @@ export const asyncRoutes = [
   adminRoutes,
   projectRoutes,
   initiatorRoutes,
-  formulatorRoutes,
+  // formulatorRoutes,
   lukRoutes,
   tukRoutes,
   expertBankRoutes,
