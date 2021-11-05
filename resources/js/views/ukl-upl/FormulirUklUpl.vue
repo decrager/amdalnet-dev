29b1ee8571d810bc68838a5ca8f4d92143b1ef02
Achmad Giovani <achmad.giovani@imaniprima.com>
2021-11-05 16:07:40 +0700

    Matriks Identifikasi Dampak

--- resources/js/views/ukl-upl/FormulirUklUpl.vue
+++ resources/js/views/ukl-upl/FormulirUklUpl.vue
@@ -1,44 +1,48 @@
 <template>
-  <div class="app-container">
-    <el-form
-      ref="postForm"
-      :model="postForm"
-      label-position="top"
-      label-width="200px"
-    >
-      <vsa-list>
-        <vsa-item>
-          <vsa-heading>
-            RONA LINGKUNGAN AWAL
-          </vsa-heading>
-          <vsa-content>
-            <rona-lingkungan-awal
-              @handleSaveRonaAwalData="handleSaveRonaAwalData"
-              @handleSaveComponents="handleSaveComponents"
-              @handleSaveRonaAwals="handleSaveRonaAwals"
-              @handleUpdateComponents="handleUpdateComponents"
-              @handleUpdateRonaAwals="handleUpdateRonaAwals"
-            />
-          </vsa-content>
-        </vsa-item>
-        <vsa-item>
-          <vsa-heading>
-            MATRIKS IDENTIFIKASI DAMPAK
-          </vsa-heading>
-          <vsa-content>
-            This is the content
-          </vsa-content>
-        </vsa-item>
-        <vsa-item>
-          <vsa-heading>
-            JENIS DAN BESARAN DAMPAK
-          </vsa-heading>
-          <vsa-content>
-            This is the content
-          </vsa-content>
-        </vsa-item>
-      </vsa-list>
-    </el-form>
+  <div class="app-container" style="padding: 24px">
+    <el-card>
+      <el-form
+        ref="postForm"
+        :model="postForm"
+        label-position="top"
+        label-width="200px"
+      >
+        <vsa-list>
+          <vsa-item>
+            <vsa-heading>
+              RONA LINGKUNGAN AWAL
+            </vsa-heading>
+            <vsa-content>
+              <rona-lingkungan-awal
+                @handleSaveRonaAwalData="handleSaveRonaAwalData"
+                @handleSaveComponents="handleSaveComponents"
+                @handleSaveRonaAwals="handleSaveRonaAwals"
+                @handleUpdateComponents="handleUpdateComponents"
+                @handleUpdateRonaAwals="handleUpdateRonaAwals"
+              />
+            </vsa-content>
+          </vsa-item>
+          <vsa-item>
+            <vsa-heading>
+              MATRIKS IDENTIFIKASI DAMPAK
+            </vsa-heading>
+            <vsa-content>
+              <matrik-identifikasi-dampak
+                :id-project="postForm.id_project"
+              />
+            </vsa-content>
+          </vsa-item>
+          <vsa-item>
+            <vsa-heading>
+              JENIS DAN BESARAN DAMPAK
+            </vsa-heading>
+            <vsa-content>
+              This is the content
+            </vsa-content>
+          </vsa-item>
+        </vsa-list>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
@@ -52,6 +56,7 @@ import {
 } from 'vue-simple-accordion';
 import 'vue-simple-accordion/dist/vue-simple-accordion.css';
 import RonaLingkunganAwal from './components/RonaLingkunganAwal.vue';
+import MatrikIdentifikasiDampak from './components/MatrikIdentifikasiDampak.vue';
 
 export default {
   name: 'FormulirUklUpl',
@@ -61,6 +66,7 @@ export default {
     VsaHeading,
     VsaContent,
     RonaLingkunganAwal,
+    MatrikIdentifikasiDampak,
   },
   data() {
     return {
@@ -140,5 +146,14 @@ export default {
   color: var(--vsa-text-color);
   background-color: var(--vsa-bg-color);
 }
-.vsa-list [hidden]{display:none}
+.vsa-list [hidden]{
+  display:none
+}
+
+.vsa-item__content{
+  margin:0;
+  padding:var(--vsa-content-padding);
+  overflow: auto;
+}
+
 </style>
---
+++ resources/js/views/ukl-upl/components/MatrikIdentifikasiDampak.vue
@@ -0,0 +1,149 @@
+<template>
+  <div style="font-size: 10pt;">
+    <table>
+      <tr class="tr-header">
+        <td v-for="comp of header[0]" :key="comp.id" :colspan="comp.colspan" :rowspan="comp.rowspan" align="center" class="td-header">
+          <span>{{ comp.name }}</span>
+        </td>
+      </tr>
+      <tr class="tr-header">
+        <td v-for="comp of header[1]" :key="comp.id" style="width: 100px;" class="td-header">
+          <span>{{ comp.name }}</span>
+        </td>
+      </tr>
+      <tr v-for="r of checked" :key="r.id" class="tr-data">
+        <td class="td-data">{{ r.name }}</td>
+        <td v-for="c of r.sub" :key="c.id" style="width: 100px;" align="center" class="td-data">
+          <input v-model="c.checked" type="checkbox">
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+import Resource from '@/api/resource';
+const prjStageResource = new Resource('project-stages');
+const componentResource = new Resource('components');
+const ronaAwalResource = new Resource('rona-awals');
+const impactIdtResource = new Resource('impact-identifications');
+
+export default {
+  name: 'MatrikIdentifikasiDampak',
+  props: {
+    idProject: {
+      type: Number,
+      default: () => 0,
+    },
+  },
+  data() {
+    return {
+      header: [],
+      projectStages: [],
+      components: [],
+      ronaAwals: [],
+      impacts: [],
+      checked: [], // matriks
+      data: {},
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    async getChecked() {
+      const dataArray = [];
+      this.ronaAwals.map((r) => {
+        const subDataArray = [];
+        this.components.map((c) => {
+          var checked = false;
+          this.impacts.map((i) => {
+            if (i.id_rona_awal === r.id && i.id_component === c.id){
+              checked = true;
+            }
+          });
+          subDataArray.push({
+            id: c.id,
+            checked: checked,
+          });
+        });
+        dataArray.push({
+          id: r.id,
+          name: r.name,
+          sub: subDataArray,
+        });
+      });
+      this.checked = dataArray;
+    },
+    sortComponents(comps) {
+      const ordered = [];
+      var stageIds = [4, 1, 2, 3];
+      stageIds.map((id) => {
+        comps.map((c) => {
+          if (c.id_project_stage === id){
+            ordered.push(c);
+          }
+        });
+      });
+      return ordered;
+    },
+    async getData() {
+      const { data } = await prjStageResource.list({});
+      this.projectStages = data;
+      const listC = await componentResource.list({
+        all: true,
+      });
+      this.components = this.sortComponents(listC.data);
+      const listR = await ronaAwalResource.list({
+        all: true,
+      });
+      this.ronaAwals = listR.data;
+      const iList = await impactIdtResource.list({
+        id_project: this.id_project,
+      });
+      this.impacts = iList.data;
+      const dataRow1 = [
+        {
+          id: 0,
+          name: 'Komponen Lingkungan/Sumber Dampak',
+          rowspan: 2,
+        },
+      ];
+      this.projectStages.map((s) => {
+        var numComps = 0;
+        this.components.map((c) => {
+          if (c.id_project_stage === s.id){
+            numComps++;
+          }
+        });
+        s['rowspan'] = 1;
+        s['colspan'] = numComps;
+        dataRow1.push(s);
+      });
+      this.header = [
+        dataRow1,
+        this.components,
+      ];
+      this.getChecked();
+    },
+  },
+};
+</script>
+
+<style scoped>
+table {
+  border-collapse: collapse;
+}
+.tr-header {
+  border: 1px solid white;
+  background-color: #3AB06F;
+  color: white;
+}
+.td-header {
+  border: 1px solid white;
+  padding: 10px;
+}
+.tr-data, .td-data {
+  border: 1px solid gray;
+  padding: 10px;
+}
+</style>
