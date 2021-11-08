43c5c5361c228d99012596dbc629efb223a06756
Achmad Giovani <achmad.giovani@imaniprima.com>
2021-11-08 09:21:16 +0700

    modify change_types (soft delete)

--- app/Entity/ChangeType.php
+++ app/Entity/ChangeType.php
@@ -4,10 +4,12 @@ namespace App\Entity;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ChangeType extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $table = 'change_types';
 
