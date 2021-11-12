81535adb4d813f0fb8713faa29fa19689433e3ab
Achmad Giovani <achmad.giovani@imaniprima.com>
2021-11-12 10:33:27 +0700

    restructure vue views for Besaran Dampak

---
+++ resources/js/views/ukl-upl/components/IdentifikasiDampakTable.vue
@@ -0,0 +1,189 @@
+<template>
+  <table>
+    <tr class="tr-header">
+      <td class="td-header">
+        <span>Komponen Dampak</span>
+      </td>
+      <td class="td-header">
+        <span>Komponen Rona Lingkungan Awal</span>
+      </td>
+      <td class="td-header" width="130">
+        <span />
+      </td>
+      <td class="td-header">
+        <span>Sumber Dampak</span>
+      </td>
+      <td class="td-header">
+        <span>Besaran Dampak</span>
+      </td>
+    </tr>
+    <tr v-for="impact of data" :key="impact.id" class="tr-data">
+      <td v-if="impact.is_stage" colspan="5" class="td-data">
+        <span>{{ impact.project_stage_name }}</span>
+      </td>
+      <td v-if="!impact.is_stage" class="td-data">
+        <el-select
+          v-model="impact.id_change_type"
+          placeholder="Perubahan"
+          style="width: 100%"
+        >
+          <el-option
+            v-for="item of changeTypeOptions"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"
+          />
+        </el-select>
+      </td>
+      <td v-if="!impact.is_stage" class="td-data">
+        <span>{{ impact.rona_awal_name }}</span>
+      </td>
+      <td v-if="!impact.is_stage" class="td-data">
+        <span>akibat</span>
+      </td>
+      <td v-if="!impact.is_stage" class="td-data">
+        <span>{{ impact.component_name }}</span>
+      </td>
+      <td v-if="!impact.is_stage" class="td-data">
+        <span>
+          <el-input v-model="impact.nominal" width="65" />
+          <el-select
+            v-model="impact.id_unit"
+            placeholder="unit"
+            style="width: 100%"
+            width="65"
+          >
+            <el-option
+              v-for="item of unitOptions"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
+            />
+          </el-select>
+        </span>
+      </td>
+    </tr>
+  </table>
+</template>
+<script>
+import Resource from '@/api/resource';
+const changeTypeResource = new Resource('change-types');
+const projectStageResource = new Resource('project-stages');
+const unitResource = new Resource('units');
+const impactIdtResource = new Resource('impact-identifications');
+
+export default {
+  name: 'IdentifikasiDampakTable',
+  props: {
+    idProject: {
+      type: Number,
+      default: () => 0,
+    },
+  },
+  data() {
+    return {
+      data: [],
+      changeTypeOptions: [],
+      unitOptions: [],
+      projectStages: [],
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    createDataArray(imps, stages) {
+      var stageIds = [4, 1, 2, 3];
+      const data = [];
+      stageIds.map((id) => {
+        const impacts = [];
+        imps.map((imp) => {
+          if (imp.id_project_stage === id){
+            impacts.push(imp);
+          }
+        });
+        var projectStageName = '';
+        stages.map((s) => {
+          if (s.id === id) {
+            projectStageName = s.name;
+          }
+        });
+        data.push({
+          id_project_stage: id,
+          project_stage_name: projectStageName,
+          impacts: impacts,
+        });
+      });
+      const dataFlat = [];
+      var dummyId = 9990;
+      data.map((d) => {
+        dataFlat.push({
+          id: dummyId,
+          is_stage: true,
+          project_stage_name: d.project_stage_name,
+        });
+
+        d.impacts.map((imp) => {
+          dataFlat.push({
+            id: imp.id,
+            is_stage: false,
+            id_change_type: imp.id_change_type,
+            component_name: imp.component_name,
+            rona_awal_name: imp.rona_awal_name,
+            nominal: imp.nominal,
+            id_unit: imp.id_unit,
+            unit_name: imp.unit_name,
+          });
+        });
+        dummyId++;
+      });
+      return dataFlat;
+    },
+    async getData() {
+      const changeTypeList = await changeTypeResource.list({});
+      this.changeTypeOptions = changeTypeList.data;
+      const unitList = await unitResource.list({});
+      this.unitOptions = unitList.data;
+      const prjStageList = await projectStageResource.list({});
+      this.projectStages = prjStageList.data;
+      const impactList = await impactIdtResource.list({
+        id_project: this.idProject,
+        join_tables: true,
+      });
+      impactList.data.map((imp) => {
+        if (imp.id_project_stage === null) {
+          imp.id_project_stage = imp.id_project_stage_master;
+        }
+        if (imp.component_name === null) {
+          imp.component_name = imp.component_name_master;
+        }
+        if (imp.rona_awal_name === null) {
+          imp.rona_awal_name = imp.rona_awal_name_master;
+        }
+      });
+      this.data = this.createDataArray(impactList.data, this.projectStages);
+      this.$emit('handleSetData', this.data);
+    },
+  },
+};
+</script>
+
+<style scoped>
+table {
+  border-collapse: collapse;
+  font-size: 14px;
+}
+.tr-header {
+  border: 1px solid white;
+  background-color: #3AB06F;
+  color: white;
+}
+.td-header {
+  border: 1px solid white;
+  padding: 10px;
+}
+.tr-data, .td-data {
+  border: 1px solid gray;
+  padding: 10px;
+}
+</style>
