fc927e03af8c8dfdd0d97d8cd701c36ec99ff304
Salman Said Ramadhan <42852021+salmansaidr@users.noreply.github.com>
2021-11-15 21:13:36 +0700

    fixing too much request

--- resources/js/views/pengujian-rkl-rpl/index.vue
+++ resources/js/views/pengujian-rkl-rpl/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="app-container">
     <el-card>
-      <el-tabs type="card">
-        <el-tab-pane label="Verifikasi & Rapat">
-          <VerifikasiRapat />
+      <el-tabs v-model="activeName" type="card">
+        <el-tab-pane label="Verifikasi & Rapat" name="verifikasi">
+          <VerifikasiRapat v-if="activeName === 'verifikasi'" />
         </el-tab-pane>
-        <el-tab-pane label="Berita Acara">
-          <BeritaAcara />
+        <el-tab-pane label="Berita Acara" name="beritaacara">
+          <BeritaAcara v-if="activeName === 'beritaacara'" />
+        </el-tab-pane>
+        <el-tab-pane label="Uji Kelayakan" name="ujikelayakan">
+          <UjiKelayakan v-if="activeName === 'ujikelayakan'" />
         </el-tab-pane>
       </el-tabs>
     </el-card>
@@ -16,12 +19,19 @@
 <script>
 import VerifikasiRapat from '@/views/pengujian-rkl-rpl/components/verifikasiRapat/index';
 import BeritaAcara from '@/views/pengujian-rkl-rpl/components/beritaAcara/index';
+import UjiKelayakan from '@/views/pengujian-rkl-rpl/components/ujiKelayakan/index';
 
 export default {
   name: 'PengujianRKLRPL',
   components: {
     VerifikasiRapat,
     BeritaAcara,
+    UjiKelayakan,
+  },
+  data() {
+    return {
+      activeName: 'verifikasi',
+    };
   },
 };
 </script>
