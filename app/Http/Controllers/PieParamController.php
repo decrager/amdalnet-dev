0826bbde5ead207d6fa96f3d368c0f9811d165f8
Husnul Hasanah <husnul.hasanah@gmail.com>
2021-11-24 12:53:48 +0700

    controller untuk pie param

---
+++ app/Http/Controllers/PieParamController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Entity\MasterPotentialImpactEvaluationParam;
+use Illuminate\Http\Request;
+
+class PieParamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Entity\MasterPotentialImpactEvaluationParam  $masterPotentialImpactEvaluationParam
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MasterPotentialImpactEvaluationParam $masterPotentialImpactEvaluationParam)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Entity\MasterPotentialImpactEvaluationParam  $masterPotentialImpactEvaluationParam
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MasterPotentialImpactEvaluationParam $masterPotentialImpactEvaluationParam)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Entity\MasterPotentialImpactEvaluationParam  $masterPotentialImpactEvaluationParam
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MasterPotentialImpactEvaluationParam $masterPotentialImpactEvaluationParam)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Entity\MasterPotentialImpactEvaluationParam  $masterPotentialImpactEvaluationParam
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MasterPotentialImpactEvaluationParam $masterPotentialImpactEvaluationParam)
+    {
+        //
+    }
+}
