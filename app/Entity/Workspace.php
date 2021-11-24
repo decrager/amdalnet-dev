8ca90a24778f5974df28c82727298a5ca8ad4485
M Zhuhriansyah Rahman <izhur@zamrudtechnology.co.id>
2021-11-25 06:05:42 +0700

    update integration workspace editor

--- app/Entity/Workspace.php
+++ app/Entity/Workspace.php
@@ -3,10 +3,33 @@
 namespace App\Entity;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 use PhpOffice\PhpWord\IOFactory;
+use App\Utils\Jwt;
+use App\Utils\Document;
 
 class Workspace
 {
+    private $_trackerStatus = [
+        0 => 'NotFound',
+        1 => 'Editing',
+        2 => 'MustSave',
+        3 => 'Corrupted',
+        4 => 'Closed',
+        6 => 'MustForceSave',
+        7 => 'CorruptedForceSave'
+    ];
+
+    /**
+     * Get Track statyus
+     * 
+     * @param Int $status
+     * @return 
+     */
+    public function getTrackstatus($status)
+    {
+        return $this->_trackerStatus[$status];
+    }
 
     /**
      * JSON Tree from headings
@@ -145,5 +168,204 @@ class Workspace
         }
 
         return null;
+    }
+
+
+    /**
+     * send command to document server editor
+     * 
+     * @param string $method
+     * @param string $key
+     * @return string 
+     */
+    public function commandRequest($method, $key) 
+    {
+        $docCommandUrl = env('OFFICE_COMMAND_URL', 'http://localhost:8008/coauthoring/CommandService.ashx');
+
+        $arr = [
+            "c" => $method,
+            "key" => $key
+        ];
+
+        $headerToken = "";
+        if (Jwt::isEnabled()) {
+            $headerToken = Jwt::encode([ "payload" => $arr ]);  // encode a payload object into a header token
+            $arr["token"] = Jwt::encode($arr);  // encode a payload object into a body token
+        }
+
+        $data = json_encode($arr);
+
+        $opts = array('http' => array(
+            'method'  => 'POST',
+            'header'=> "Content-type: application/json\r\n" .
+                (empty($headerToken) ? "" : "Authorization: Bearer $headerToken\r\n"),  // add a header Authorization with a header token and Authorization prefix in it
+            'content' => $data
+        ));
+
+        if (substr($documentCommandUrl, 0, strlen("https")) === "https") {
+           $opts['ssl'] = array( 'verify_peer'   => FALSE );
+        }
+
+        $context = stream_context_create($opts);
+        $response_data = file_get_contents($documentCommandUrl, FALSE, $context);
+
+        return $response_data;
+    }
+
+    /**
+     * process save document
+     * 
+     * @param array|mixed $data
+     * @param string $fileName
+     * @return array|mixed $userAddress 
+     */
+    public function processSave($data, $fileName, $userAddress)
+    {
+        $downloadUri = $data["url"];
+        if ($downloadUri === null) {
+            $result["error"] = 1;
+            return $result;
+        }
+
+        $curExt = strtolower('.' . pathinfo($fileName, PATHINFO_EXTENSION));  // get current file extension
+        $downloadExt = strtolower('.' . pathinfo($downloadUri, PATHINFO_EXTENSION));  // get the extension of the downloaded file
+        $newFileName = $fileName;
+
+        // convert downloaded file to the file with the current extension if these extensions aren't equal
+        if ($downloadExt != $curExt) {
+            $key = Document::GenerateRevisionId($downloadUri);
+
+            try {
+                Log::info('Convert ' . $downloadUri . ' from ' . $downloadExt . ' to ' . $curExt);
+                $convertedUri;  // convert file and give url to a new file
+                $percent = Document::GetConvertedUri($downloadUri, $downloadExt, $curExt, $key, FALSE, $convertedUri);
+                if (!empty($convertedUri)) {
+                    $downloadUri = $convertedUri;
+                } else {
+                    Log::info("   Convert after save convertedUri is empty");
+                    $baseNameWithoutExt = substr($fileName, 0, strlen($fileName) - strlen($curExt));
+                    $newFileName = Document::GetCorrectName($baseNameWithoutExt . $downloadExt, $userAddress);  // get the correct file name if it already exists
+                }
+            } catch (Exception $e) {
+                Log::error("   Convert after save ".$e->getMessage());
+                $baseNameWithoutExt = substr($fileName, 0, strlen($fileName) - strlen($curExt));
+                $newFileName = Docment::GetCorrectName($baseNameWithoutExt . $downloadExt, $userAddress);
+            }
+        }
+
+        $saved = 1;
+
+        if (!(($new_data = file_get_contents($downloadUri)) === FALSE)) {
+            $storagePath = Document::getStoragePath($newFileName, $userAddress);  // get the file path
+            $histDir = Document::getHistoryDir($storagePath);  // get the path to the history directory
+            $verDir = Document::getVersionDir($histDir, Document::getFileVersion($histDir));  // get the path to the file version
+    
+            mkdir($verDir);  // if the path doesn't exist, create it
+    
+            rename(Document::getStoragePath($fileName, $userAddress), $verDir . DIRECTORY_SEPARATOR . "prev" . $curExt);  // get the path to the previous file version and rename the storage path with it
+            file_put_contents($storagePath, $new_data, LOCK_EX);  // save file to the storage directory
+    
+            if ($changesData = file_get_contents($data["changesurl"])) {
+                file_put_contents($verDir . DIRECTORY_SEPARATOR . "diff.zip", $changesData, LOCK_EX);  // save file changes to the diff.zip archive
+            }
+    
+            $histData = $data["changeshistory"];
+            if (empty($histData)) {
+                $histData = json_encode($data["history"], JSON_PRETTY_PRINT);
+            }
+            if (!empty($histData)) {
+                file_put_contents($verDir . DIRECTORY_SEPARATOR . "changes.json", $histData, LOCK_EX);  // write the history changes to the changes.json file
+            }
+            file_put_contents($verDir . DIRECTORY_SEPARATOR . "key.txt", $data["key"], LOCK_EX);  // write the key value to the key.txt file
+    
+            $forcesavePath = Document::getForcesavePath($newFileName, $userAddress, false);  // get the path to the forcesaved file version
+            if ($forcesavePath != "") {  // if the forcesaved file version exists
+                unlink($forcesavePath);  // remove it
+            }
+    
+            $saved = 0;
+        }
+    
+        $result["error"] = $saved;
+    
+        return $result;        
+    }
+
+    /**
+     * process force save document
+     * 
+     * @param array|mixed $data
+     * @param string $fileName
+     * @return array|mixed $userAddress 
+     */
+    function processForceSave($data, $fileName, $userAddress) {
+        $downloadUri = $data["url"];
+        if ($downloadUri === null) {
+            $result["error"] = 1;
+            return $result;
+        }
+    
+        $curExt = strtolower('.' . pathinfo($fileName, PATHINFO_EXTENSION));  // get current file extension
+        $downloadExt = strtolower('.' . pathinfo($downloadUri, PATHINFO_EXTENSION));  // get the extension of the downloaded file
+        $newFileName = false;
+    
+        // convert downloaded file to the file with the current extension if these extensions aren't equal
+        if ($downloadExt != $curExt) {
+            $key = Document::GenerateRevisionId($downloadUri);
+    
+            try {
+                Log::debug("   Convert " . $downloadUri . " from " . $downloadExt . " to " . $curExt);
+                $convertedUri;  // convert file and give url to a new file
+                $percent = Document::GetConvertedUri($downloadUri, $downloadExt, $curExt, $key, FALSE, $convertedUri);
+                if (!empty($convertedUri)) {
+                    $downloadUri = $convertedUri;
+                } else {
+                    Log::debug("   Convert after save convertedUri is empty");
+                    $baseNameWithoutExt = substr($fileName, 0, strlen($fileName) - strlen($curExt));
+                    $newFileName = true;
+                }
+            } catch (Exception $e) {
+                Log::error("   Convert after save ".$e->getMessage());
+                $newFileName = true;
+            }
+        }
+    
+        $saved = 1;
+    
+        if (!(($new_data = file_get_contents($downloadUri)) === FALSE)) {
+            $baseNameWithoutExt = substr($fileName, 0, strlen($fileName) - strlen($curExt));
+            $isSubmitForm = $data["forcesavetype"] == 3;  // SubmitForm
+    
+            if ($isSubmitForm) {
+                if ($newFileName){
+                    $fileName = Document::GetCorrectName($baseNameWithoutExt . "-form" . $downloadExt, $userAddress);  // get the correct file name if it already exists
+                } else {
+                    $fileName = Document::GetCorrectName($baseNameWithoutExt . "-form" . $curExt, $userAddress);
+                }
+                $forcesavePath = Document::getStoragePath($fileName, $userAddress);
+            } else {
+                if ($newFileName){
+                    $fileName = Document::GetCorrectName($baseNameWithoutExt . $downloadExt, $userAddress);
+                }
+                // create forcesave path if it doesn't exist
+                $forcesavePath = Document::getForcesavePath($fileName, $userAddress, false);
+                if ($forcesavePath == "") {
+                    $forcesavePath = Document::getForcesavePath($fileName, $userAddress, true);
+                }
+            }
+    
+            file_put_contents($forcesavePath, $new_data, LOCK_EX);
+    
+            if ($isSubmitForm) {
+                $uid = $data["actions"][0]["userid"];  // get the user id
+                self::createMeta($fileName, $uid, "Filling Form", $userAddress);  // create meta data for the forcesaved file
+            }
+    
+            $saved = 0;
+        }
+    
+        $result["error"] = $saved;
+    
+        return $result;
     }
 }
--- app/Http/Controllers/WorkspaceController.php
+++ app/Http/Controllers/WorkspaceController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use App\Entity\Workspace;
+use Illuminate\Support\Facades\Log;
 
 class WorkspaceController extends Controller
 {
@@ -100,9 +101,10 @@ class WorkspaceController extends Controller
             'documentType' => 'word',
             'document' => [
                 'fileType' => 'docx',
-                'key' => '-1472914638',
+                'key' => 'keyaaaa',
                 'title' => 'UKL UPL SPBU - Edit Nafila_edit FM.docx',
-                'url' => $officeUrl.'/example/download?fileName=UKL%20UPL%20SPBU%20-%20Edit%20Nafila_edit%20FM.docx&useraddress=36.72.20.141__172.17.0.1',
+                // 'url' => $appUrl.'/workspace/document/download?fileName=61943e88ad99a.docx',
+                'url' => $appUrl.'/storage/workspace/61943e88ad99a.docx',
                 'permissions' => [
                     'fillForms' => true,
                     'edit' => true,
@@ -141,17 +143,17 @@ class WorkspaceController extends Controller
                     'logo' => [
                         'image' => $appUrl.'/images/logo-amdal-white.png',
                         'imageEmbedded' => $appUrl.'/images/logo-amdal-white.png',
-                        'url' => '',
+                        'url' => $appUrl.'/images/logo-amdal-white.png',
                     ],
                 ],
-                'callbackUrl' => $officeUrl.'/example/track?filename=UKL%20UPL%20SPBU%20-%20Edit%20Nafila_edit%20FM.docx&useraddress=36.72.20.141__172.17.0.1',
+                'callbackUrl' => $appUrl.'/workspace/document/track?filename=61943e88ad99a.docx',
             ],
         ];
         return response()->json($config);
     }
 
     /**
-     * Track for workspace editor
+     * Track from workspace editor
      *
      * @param Request $request
      * @param String $id
@@ -159,8 +161,41 @@ class WorkspaceController extends Controller
      */
     public function track(Request $request)
     {
-        $result["error"] = 0;
-        // $data = $request->
+        Log::debug('Track DOC: '. serialize($request->query()));
+        $workspace = new Workspace();
+        $result['error'] = 0;
+        $data = $request->json();
+        if ($data['error']) {
+            return $data;
+        }
+
+        $status = $workspace->getTrackStatus($data['status']);
+
+        $userAddress = $request->query('userAddress');
+        $fileName = basename($request->query('fileName'));
+
+        switch ($status) {
+            case 'Editing':  // status == 1
+                if ($data['actions'] && $data['actions'][0]['type'] == 0) {   // finished edit
+                    $user = $data['actions'][0]['userid'];  // the user who finished editing
+                    if (array_search($user, $data['users']) === FALSE) {
+                        $commandRequest = $workspace->commandRequest('forcesave', $data['key']);  // create a command request with the forcasave method
+                        Log::debug('CommandRequest forcesave: ' . serialize($commandRequest));
+                    }
+                }
+                break;
+            case "MustSave":  // status == 2
+            case "Corrupted":  // status == 3
+                $result = $workspace->processSave($data, $fileName, $userAddress);
+                break;
+            case "MustForceSave":  // status == 6
+            case "CorruptedForceSave":  // status == 7
+                $result = $workspace->processForceSave($data, $fileName, $userAddress);
+                break;
+        }
+
+        $result['status'] = 'success';
+        response()->json($result);
     }
 
     /**
@@ -182,7 +217,8 @@ class WorkspaceController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function download(Request $request)
-    {   
+    {
+
     }
 
     /**
@@ -204,6 +240,31 @@ class WorkspaceController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function delete(Request $request)
-    {   
+    {
+
+    }
+
+    /**
+     * Files list files on workspace server
+     *
+     * @param Request $request
+     * @param String $id
+     * @return \Illuminate\Http\Response
+     */
+    public function files(Request $request)
+    {
+        
+    }
+
+    /**
+     * Assets list files on workspace server
+     *
+     * @param Request $request
+     * @param String $id
+     * @return \Illuminate\Http\Response
+     */
+    public function assets(Request $request)
+    {
+        
     }
 }
