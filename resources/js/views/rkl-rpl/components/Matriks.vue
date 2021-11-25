fb1be926d940d5ee2d7e4fc43010c08cb9cd502e
Salman Said Ramadhan <42852021+salmansaidr@users.noreply.github.com>
2021-11-25 23:38:08 +0700

    fixing bugs dokumen rkl rpl

--- resources/js/views/rkl-rpl/components/Matriks.vue
+++ resources/js/views/rkl-rpl/components/Matriks.vue
@@ -82,22 +82,38 @@ export default {
             linebreaks: true,
           });
           doc.render({
-            a_pra_konstruksi_rkl: this.rkl.poinA[0].data,
-            a_konstruksi_rkl: this.rkl.poinA[1].data,
-            a_operasi_rkl: this.rkl.poinA[2].data,
-            a_pasca_operasi_rkl: this.rkl.poinA[3].data,
-            b_pra_konstruksi_rkl: this.rkl.poinB[0].data,
-            b_konstruksi_rkl: this.rkl.poinB[1].data,
-            b_operasi_rkl: this.rkl.poinB[2].data,
-            b_pasca_operasi_rkl: this.rkl.poinB[3].data,
-            a_pra_konstruksi_rpl: this.rpl.poinA[0].data,
-            a_konstruksi_rpl: this.rpl.poinA[1].data,
-            a_operasi_rpl: this.rpl.poinA[2].data,
-            a_pasca_operasi_rpl: this.rpl.poinA[3].data,
-            b_pra_konstruksi_rpl: this.rpl.poinB[0].data,
-            b_konstruksi_rpl: this.rpl.poinB[1].data,
-            b_operasi_rpl: this.rpl.poinB[2].data,
-            b_pasca_operasi_rpl: this.rpl.poinB[3].data,
+            a_pra_konstruksi_rkl:
+              this.rkl.poinA.length > 0 ? this.rkl.poinA[0].data : [],
+            a_konstruksi_rkl:
+              this.rkl.poinA.length > 0 ? this.rkl.poinA[1].data : [],
+            a_operasi_rkl:
+              this.rkl.poinA.length > 0 ? this.rkl.poinA[2].data : [],
+            a_pasca_operasi_rkl:
+              this.rkl.poinA.length > 0 ? this.rkl.poinA[3].data : [],
+            b_pra_konstruksi_rkl:
+              this.rkl.poinB.length > 0 ? this.rkl.poinB[0].data : [],
+            b_konstruksi_rkl:
+              this.rkl.poinB.length > 0 ? this.rkl.poinB[1].data : [],
+            b_operasi_rkl:
+              this.rkl.poinB.length > 0 ? this.rkl.poinB[2].data : [],
+            b_pasca_operasi_rkl:
+              this.rkl.poinB.length > 0 ? this.rkl.poinB[3].data : [],
+            a_pra_konstruksi_rpl:
+              this.rpl.poinA.length > 0 ? this.rpl.poinA[0].data : [],
+            a_konstruksi_rpl:
+              this.rpl.poinA.length > 0 ? this.rpl.poinA[1].data : [],
+            a_operasi_rpl:
+              this.rpl.poinA.length > 0 ? this.rpl.poinA[2].data : [],
+            a_pasca_operasi_rpl:
+              this.rpl.poinA.length > 0 ? this.rpl.poinA[3].data : [],
+            b_pra_konstruksi_rpl:
+              this.rpl.poinB.length > 0 ? this.rpl.poinB[0].data : [],
+            b_konstruksi_rpl:
+              this.rpl.poinB.length > 0 ? this.rpl.poinB[1].data : [],
+            b_operasi_rpl:
+              this.rpl.poinB.length > 0 ? this.rpl.poinB[2].data : [],
+            b_pasca_operasi_rpl:
+              this.rpl.poinB.length > 0 ? this.rpl.poinB[3].data : [],
           });
 
           const out = doc.getZip().generate({
