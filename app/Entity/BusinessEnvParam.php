2f27719faa4524f659ff8b04f5c4c54b6020045a
Achmad Giovani <achmad.giovani@imaniprima.com>
2021-11-26 21:33:54 +0700

    controllers & resources from business & business_env_params

---
+++ app/Entity/BusinessEnvParam.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Entity;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BusinessEnvParam extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'business_id',
+        'id_param',
+        'condition',
+        'id_unit',
+        'doc_req',
+        'amdal_type',
+        'risk_level',
+        'is_param_multisector',
+    ];
+
+    public function param(){
+        return $this->hasOne(Param::class, 'id', 'id_param');
+    }
+
+    public function unit(){
+        return $this->hasOne(Unit::class, 'id', 'id_unit');
+    }
+}
---
+++ app/Http/Controllers/BusinessEnvParamController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Entity\BusinessEnvParam;
+use Illuminate\Http\Request;
+
+class BusinessEnvParamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Entity\BusinessEnvParam  $businessEnvParam
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BusinessEnvParam $businessEnvParam)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Entity\BusinessEnvParam  $businessEnvParam
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BusinessEnvParam $businessEnvParam)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Entity\BusinessEnvParam  $businessEnvParam
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BusinessEnvParam $businessEnvParam)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Entity\BusinessEnvParam  $businessEnvParam
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BusinessEnvParam $businessEnvParam)
+    {
+        //
+    }
+}
