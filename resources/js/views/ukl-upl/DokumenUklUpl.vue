e3dcdbf41ca17d2d5f2b670c1b24111555a3d240
klittdj <danudenny@gmail.com>
2021-11-29 09:11:59 +0700

    fix view dokumen ukl upl

--- resources/js/views/ukl-upl/DokumenUklUpl.vue
+++ resources/js/views/ukl-upl/DokumenUklUpl.vue
@@ -133,8 +133,8 @@ export default {
       );
     },
     showDocs() {
-      this.showDocument = true;
       this.generateDoc();
+      this.showDocument = true;
     },
     formatDate(value) {
       if (value) {
