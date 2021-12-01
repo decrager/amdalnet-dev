9644e25b4a59cb45b84059348a6018e84a592691
Salman Said Ramadhan <42852021+salmansaidr@users.noreply.github.com>
2021-12-01 07:55:57 +0700

    fixing spt

--- routes/api.php
+++ routes/api.php
@@ -243,3 +243,4 @@ Route::get('get-document-ka/{id}', [ExportDocument::class, 'getDocKA']);
 Route::get('form-ka-pdf/{id}', [ExportDocument::class, 'ExportKAPdf']);
 Route::apiResource('andal-clone', 'AndalCloneController');
 Route::get('map/{id}', [ProjectMapAttachmentController::class, 'show']);
+Route::apiResource('public-spt', 'PublicSPTController');
