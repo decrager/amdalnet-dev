c17990bae1adf48721ed74aef2d82add112556ae
Cahyo <cahyo.adi.permono@gmail.com>
2021-12-05 14:39:23 +0700

    bugfix change button to done, add new logo

--- resources/js/layout/components/Sidebar/Logo.vue
+++ resources/js/layout/components/Sidebar/Logo.vue
@@ -2,7 +2,7 @@
   <div class="sidebar-logo-container" :class="{'collapse':collapse}">
     <transition name="sidebarLogoFade">
       <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
-        <img v-if="logo" :src="logo2" class="sidebar-logo">
+        <img v-if="logo" :src="logo2" class="sidebar-logo" style="width: 40px; height: 40px">
       </router-link>
       <router-link v-else key="expand" class="sidebar-logo-link" to="/">
         <img v-if="logo" :src="logo" class="sidebar-logo">
