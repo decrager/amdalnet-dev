86978af537faf1426f8851ecd4a543a6735afd4a
Cahyo <cahyo.adi.permono@gmail.com>
2021-12-17 08:27:55 +0700

    adding enggine

--- resources/js/views/home/section/Action.vue
+++ resources/js/views/home/section/Action.vue
@@ -62,11 +62,124 @@
         </div>
       </div>
     </div>
+    <div class="simulations__box__links__wrapper">
+      <h1 style="color: white">Simulasi Penapisan Dokumen</h1>
+      <div>
+        <sub-project-table :list="listSubProject" :list-kbli="getListKbli" @cancel="cancelParam" />
+        <el-button
+          type="primary"
+          @click="handleAddSubProjectTable"
+        >+</el-button>
+      </div>
+      <h2 style="color: white; margin-top: 20px">Hasil Akhir Penapisan {{ last_result }}</h2>
+    </div>
+
+    <amdal-simulation-dialog :show="showAmdalSimulation" />
   </section>
 </template>
 
 <script>
+import SubProjectTable from '../../project/components/SubProjectTable.vue';
+import AmdalSimulationDialog from '../components/AmdalSimulationDialog.vue';
 export default {
   name: 'ActionHome',
+  components: { AmdalSimulationDialog, SubProjectTable },
+  data() {
+    return {
+      listSubProject: [],
+      showAmdalSimulation: false,
+      last_result: '',
+    };
+  },
+  computed: {
+    getListKbli() {
+      return this.$store.getters.kblis;
+    },
+  },
+  methods: {
+    cancelParam(){
+      this.calculateListSubProjectResult();
+      this.calculateChoosenProject();
+    },
+    handleAddSubProjectTable(){
+      this.listSubProject.push({});
+    },
+    sumResult(listParam){
+      let result = '';
+      let result_risk = '';
+      let scale = '';
+      let scale_unit = '';
+      let amdal_type = '';
+
+      for (let i = 0; i < listParam.length; i++) {
+        if (listParam[i].result === 'AMDAL'){
+          result = listParam[i].result;
+          result_risk = listParam[i].result_risk;
+          scale = listParam[i].scale;
+          scale_unit = listParam[i].scale_unit;
+          amdal_type = listParam[i].amdal_type;
+          break;
+        } else if (listParam[i].result === 'UKL-UPL'){
+          result = listParam[i].result;
+          result_risk = listParam[i].result_risk;
+          scale = listParam[i].scale;
+          scale_unit = listParam[i].scale_unit;
+        } else if (listParam[i].result === 'SPPL' && result !== 'UKL-UPL'){
+          result = listParam[i].result;
+          result_risk = listParam[i].result_risk;
+          scale = listParam[i].scale;
+          scale_unit = listParam[i].scale_unit;
+        }
+      }
+      return { result, result_risk, scale, scale_unit, amdal_type };
+    },
+    calculateListSubProjectResult(){
+      this.listSubProject = this.listSubProject.map(e => {
+        e.listSubProjectParamsCalc = e.listSubProjectParams.filter(e => e.used);
+
+        const { result, result_risk, scale, scale_unit, amdal_type } = this.sumResult(e.listSubProjectParamsCalc);
+        e.result = result;
+        e.result_risk = result_risk;
+        e.scale = scale;
+        e.scale_unit = scale_unit;
+        e.amdal_type = amdal_type;
+
+        return e;
+      });
+    },
+    calculateChoosenProject(){
+      // console.log('project tanpa filter', this.currentProject);
+      const listMainProjectAmdal = this.listSubProject.filter(e => e.type === 'utama' && e.result === 'AMDAL');
+      const listMainProjectUklUpl = this.listSubProject.filter(e => e.type === 'utama' && e.result === 'UKL-UPL');
+      const listMainProjectSppl = this.listSubProject.filter(e => e.type === 'utama' && e.result === 'SPPL');
+
+      // console.log('listAmdal', listMainProjectAmdal);
+      let choosenProject = '';
+
+      if (listMainProjectAmdal.length !== 0){
+        choosenProject = listMainProjectAmdal[0];
+      } else if (listMainProjectUklUpl.length !== 0) {
+        choosenProject = listMainProjectUklUpl[0];
+      } else if (listMainProjectSppl.length !== 0) {
+        choosenProject = listMainProjectSppl[0];
+      }
+
+      // console.log('choosenProject', choosenProject);
+
+      // add choosen project to current project
+      // this.kbli = choosenProject.kbli;
+      // this.required_doc = choosenProject.result;
+      // this.risk_level = choosenProject.result_risk;
+      // this.result_risk = choosenProject.result_risk;
+      // this.sector = choosenProject.sector;
+      // this.authority = 'Pusat';
+      // this.plan_type = choosenProject.name;
+      // this.scale = choosenProject.scale;
+      // this.scale_unit = choosenProject.scale_unit;
+      // this.listSubProjectParams = choosenProject.listSubProjectParams;
+
+      this.last_result = choosenProject.result + ' ' + choosenProject.amdal_type;
+    },
+  },
 };
 </script>
