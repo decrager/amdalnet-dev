562a7cd9a45c7e6b82ec2b340f5927015ffd92a5
Cahyo <cahyo.adi.permono@gmail.com>
2021-12-18 15:07:41 +0700

    fixing tampilan

--- resources/js/views/home/section/Action.vue
+++ resources/js/views/home/section/Action.vue
@@ -3,39 +3,71 @@
     <div class="actions__container container grid">
       <div class="actions__data__links">
         <h2 class="section__title actions__title__links">Layanan AMDALNET</h2>
-        <div class="actions__box__links__wrapper">
+        <div class="actions__box__links__wrapper" style="margin-bottom: 0" @click="() => showPenapisan = !showPenapisan">
           <div class="actions__box__links__icon">
             <img src="/images/list2.svg" alt="">
           </div>
           <div class="actions__box__links__desc">
-            <h2 class="actions__box__links__desc__title"><span class="title__primary">Pengajuan</span> Persetujuan Lingkungan</h2>
-            <span class="actions__box__links__desc__subtitle">Pengajuan Persetujuan lingkungan untuk usaha dan kegiatan</span>
+            <h2 class="actions__box__links__desc__title"><span class="title__primary">Penapisan</span> Otomatis</h2>
+            <!-- <span class="actions__box__links__desc__subtitle">Pengajuan Persetujuan lingkungan untuk usaha dan kegiatan</span> -->
+          </div>
+          <div>
+            <img v-show="!showPenapisan" src="/images/right-arrow.svg" alt="" style="width: 70px; height: 85px;">
+            <img v-show="showPenapisan" src="/images/down-arrow.svg" alt="" style="width: 70px; height: 85px;">
           </div>
         </div>
-
-        <router-link to="/webgis">
-          <div class="actions__box__links__wrapper">
-            <div class="actions__box__links__icon">
-              <img src="/images/map3.svg" alt="">
-            </div>
-            <div class="actions__box__links__desc">
-              <h2 class="actions__box__links__desc__title"><span class="title__primary">WEBGIS</span> AMDAL</h2>
-              <span class="actions__box__links__desc__subtitle">Peta tematik penyebaran daerah yang sudah mendapat izin AMDAL</span>
-            </div>
+        <div class="content" :hidden="!showPenapisan">
+          <h1 style="color: white">Simulasi Penapisan Dokumen</h1>
+          <div>
+            <sub-project-table :list="listSubProject" :list-kbli="getListKbli" @cancel="cancelParam" />
+            <el-button
+              type="primary"
+              @click="handleAddSubProjectTable"
+            >+</el-button>
           </div>
-        </router-link>
+          <h2 style="color: white; margin-top: 20px">Hasil Akhir Penapisan {{ last_result }}</h2>
+        </div>
 
-        <div class="actions__box__links__wrapper">
+        <div class="actions__box__links__wrapper" style="margin-top: 20px; margin-bottom: 0" @click="() => showPelingkupan = !showPelingkupan">
+          <div class="actions__box__links__icon">
+            <img src="/images/map3.svg" alt="">
+          </div>
+          <div class="actions__box__links__desc">
+            <h2 class="actions__box__links__desc__title"><span class="title__primary">Asistensi</span> Pelingkupan</h2>
+            <!-- <span class="actions__box__links__desc__subtitle">Peta tematik penyebaran daerah yang sudah mendapat izin AMDAL</span> -->
+          </div>
+          <div>
+            <img v-show="!showPelingkupan" src="/images/right-arrow.svg" alt="" style="width: 70px; height: 85px;">
+            <img v-show="showPelingkupan" src="/images/down-arrow.svg" alt="" style="width: 70px; height: 85px;">
+          </div>
+        </div>
+        <div class="content" :hidden="!showPelingkupan" style="padding-top: 20px;">
+          <h3 class="sub-title"><img src="/images/tracking.svg" style="width: 16px; height:16px" alt=""> Proses Persetujuan Lingkungan</h3>
+          <h3 class="sub-title"><img src="/images/tracking.svg" style="width: 16px; height:16px" alt=""> Pelayanan Public</h3>
+          <h3 class="sub-title"><img src="/images/tracking.svg" style="width: 16px; height:16px" alt=""> Tracking Dokumen</h3>
+        </div>
+
+        <div class="actions__box__links__wrapper" style="margin-top: 20px; margin-bottom: 0" @click="() => showDigi = !showDigi">
           <div class="actions__box__links__icon">
             <img src="/images/communication.svg" alt="">
           </div>
           <div class="actions__box__links__desc">
-            <h2 class="actions__box__links__desc__title"><span class="title__primary">Pelayanan</span> Publik</h2>
-            <span class="actions__box__links__desc__subtitle">Konsultasi untuk pengajuan izin lingkungan</span>
+            <h2 class="actions__box__links__desc__title"><span class="title__primary">AMDAL</span> Digital Workspace</h2>
+            <!-- <span class="actions__box__links__desc__subtitle">Konsultasi untuk pengajuan izin lingkungan</span> -->
+          </div>
+          <div>
+            <img v-show="!showDigi" src="/images/right-arrow.svg" alt="" style="width: 70px; height: 85px;">
+            <img v-show="showDigi" src="/images/down-arrow.svg" alt="" style="width: 70px; height: 85px;">
           </div>
         </div>
+        <div class="content" :hidden="!showDigi" style="padding-top: 20px;">
+          <h3 class="sub-title"><img src="/images/list2.svg" style="width: 16px; height:16px" alt=""> AMDAL Digital</h3>
+          <router-link to="/webgis">
+            <h3 class="sub-title"><img src="/images/map3.svg" style="width: 16px; height:16px" alt=""> WebGIS AMDAL</h3>
+          </router-link>
+        </div>
 
-        <div class="actions__box__links__wrapper">
+        <!-- <div class="actions__box__links__wrapper">
           <div class="actions__box__links__icon">
             <img src="/images/tracking.svg" alt="">
           </div>
@@ -43,7 +75,7 @@
             <h2 class="actions__box__links__desc__title"><span class="title__primary">Tracking</span> Dokumen</h2>
             <span class="actions__box__links__desc__subtitle">Layanan untuk melacak status dokumen AMDAL</span>
           </div>
-        </div>
+        </div> -->
       </div>
 
       <div class="actions__data__video">
@@ -62,7 +94,7 @@
         </div>
       </div>
     </div>
-    <div class="simulations__box__links__wrapper">
+    <!-- <div class="simulations__box__links__wrapper">
       <h1 style="color: white">Simulasi Penapisan Dokumen</h1>
       <div>
         <sub-project-table :list="listSubProject" :list-kbli="getListKbli" @cancel="cancelParam" />
@@ -72,7 +104,7 @@
         >+</el-button>
       </div>
       <h2 style="color: white; margin-top: 20px">Hasil Akhir Penapisan {{ last_result }}</h2>
-    </div>
+    </div> -->
 
     <amdal-simulation-dialog :show="showAmdalSimulation" />
   </section>
@@ -89,6 +121,9 @@ export default {
       listSubProject: [],
       showAmdalSimulation: false,
       last_result: '',
+      showPenapisan: false,
+      showPelingkupan: false,
+      showDigi: false,
     };
   },
   computed: {
@@ -183,3 +218,20 @@ export default {
   },
 };
 </script>
+<style>
+  .content {
+  padding: 0 18px;
+  overflow: hidden;
+  background-color: #20492E;
+  padding-bottom: 10px;
+  }
+
+  .sub-title {
+    color: white;
+    margin-bottom: 5px
+  }
+  .sub-title:hover {
+    cursor: pointer;
+    color: #F6993F;
+  }
+</style>
