37757ec5f24e2b1caeb179f9e0b88dfd8eb30911
Cahyo <cahyo.adi.permono@gmail.com>
2021-12-22 13:54:26 +0700

    add notification

--- resources/js/layout/components/Navbar.vue
+++ resources/js/layout/components/Navbar.vue
@@ -7,7 +7,17 @@
     <div class="right-menu">
       <template v-if="device!=='mobile'">
         <lang-select class="right-menu-item hover-effect" />
-        <i class="el-icon-bell right-menu-item hover-effect" style="cursor: pointer; font-size: 18px; vertical-align: middle;" />
+        <el-dropdown class="right-menu-item hover-effect">
+          <el-badge is-dot :hidden="sumNotifUnread > 0">
+            <el-button icon="el-icon-bell" type="text" style="cursor: pointer; font-size: 18px; vertical-align: middle; color: white" @click="markAsRead" />
+            <!-- <i class="el-icon-bell" style="cursor: pointer; font-size: 18px; vertical-align: middle;" /> -->
+          </el-badge>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item v-for="notif in notifications" :key="notif.id">
+              {{ notif.data.createdUser.name }} baru saja mendaftar dengan email {{ notif.data.createdUser.email }}
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
         <search id="header-search" class="right-menu-item" />
 
         <!-- <screenfull id="screenfull" class="right-menu-item hover-effect" />
@@ -16,7 +26,7 @@
           <size-select id="size-select" class="right-menu-item hover-effect" />
         </el-tooltip> -->
       </template>
-
+      <el-button type="text" style="cursor: default; font-size: 18px; vertical-align: 15px; margin-right: 10px; color: white;">{{ getRole }}</el-button>
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
         <div class="avatar-wrapper">
           <img :src="avatar || 'no-avatar.png'" class="user-avatar" @error="$event.target.src='no-avatar.png'">
@@ -38,13 +48,15 @@
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
-      <el-button type="text" style="cursor: default; font-size: 18px; vertical-align: 15px; margin-right: 10px; color: white;">{{ getRole }}</el-button>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+// import Echo from 'laravel-echo';
+import Resource from '@/api/resource';
+const markAsReadResource = new Resource('mark-all-read');
 import Breadcrumb from '@/components/Breadcrumb';
 import Hamburger from '@/components/Hamburger';
 // import Screenfull from '@/components/Screenfull';
@@ -68,6 +80,7 @@ export default {
       'avatar',
       'device',
       'userId',
+      'notifications',
     ]),
     getRole() {
       const roles = this.$store.getters.roles.map(value => {
@@ -82,8 +95,31 @@ export default {
       });
       return roles.join(' | ');
     },
+    sumNotifUnread(){
+      return this.notifications.filter(e => e.read_at !== null).length;
+    },
+  },
+  created(){
+    console.log(this.$store.getters.token);
+    window.Echo.join(`chat`)
+      .here((users) => {
+        //
+      })
+      .joining((user) => {
+        console.log(user.name);
+      })
+      .leaving((user) => {
+        console.log(user.name);
+      })
+      .error((error) => {
+        console.error(error);
+      });
   },
   methods: {
+    markAsRead(){
+      console.log('test');
+      markAsReadResource.get(this.userId);
+    },
     toggleSideBar() {
       this.$store.dispatch('app/toggleSideBar');
     },
@@ -94,7 +130,12 @@ export default {
   },
 };
 </script>
-
+<style>
+.el-badge__content.is-fixed.is-dot {
+  right: 5px;
+  top: 10px;
+}
+</style>
 <style lang="scss" scoped>
 .navbar {
   height: 50px;
--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ use App\Http\Controllers\LpjpController;
 use App\Http\Controllers\MatriksDampakController;
 use App\Http\Controllers\MatriksUklUplController;
 use App\Http\Controllers\WebgisController;
+use App\Laravue\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -280,3 +281,9 @@ Route::apiResource('env-manage-plans', 'EnvManagePlanController');
 Route::apiResource('env-manage-docs', 'EnvManageDocController');
 Route::apiResource('env-monitor-plans', 'EnvMonitorPlanController');
 Route::apiResource('public-questions', 'PublicQuestionController');
+
+// notification
+Route::get('mark-all-read/{user}', function(User $user){
+    $user->unreadNotifications->markAsRead();
+    return response(['message'=>'done']);
+});
