a4c3b09386f991500b2c6a27d837d9d1d3870db6
Salman Said Ramadhan <42852021+salmansaidr@users.noreply.github.com>
2021-12-30 11:39:18 +0700

    create feasibility_test_teams & feasibility_test_team_members table

---
+++ database/migrations/2021_12_30_094308_create_feasibility_test_team_members_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeasibilityTestTeamMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feasibility_test_team_members', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_feasibility_test_team')->nullable();
+            $table->string('role')->nullable();
+            $table->string('nik')->nullable();
+            $table->string('nip')->nullable();
+            $table->string('name')->nullable();
+            $table->string('institution')->nullable();
+            $table->string('email')->nullable();
+            $table->string('position')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('decision_number')->nullable();
+            $table->string('decision_file')->nullable();
+            $table->string('sex')->nullable();
+            $table->string('cv')->nullable();
+            $table->integer('id_province')->nullable();
+            $table->integer('id_district')->nullable();
+            $table->text('address')->nullable();
+            $table->timestamps();
+
+            $table->foreign('id_feasibility_test_team')->references('id')->on('feasibility_test_teams')->onDelete('cascade');
+            $table->foreign('id_province')->references('id')->on('provinces')->onDelete('cascade');
+            $table->foreign('id_district')->references('id')->on('districts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feasibility_test_team_members');
+    }
+}
