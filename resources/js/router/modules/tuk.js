5a98feb42aefd2308445aa1dc3918d1849af09b1
Salman Said Ramadhan <42852021+salmansaidr@users.noreply.github.com>
2022-01-03 14:29:02 +0700

    manajemen tuk

--- resources/js/router/modules/tuk.js
+++ resources/js/router/modules/tuk.js
@@ -7,9 +7,39 @@ const tukRoutes = {
   children: [
     {
       path: '',
-      component: () => import('@/views/error-page/404'),
+      component: () => import('@/views/tuk-management/index.vue'),
       name: 'tuk',
-      meta: { title: 'tuk', icon: 'zip', noCache: true, permissions: ['view menu examiner team'] },
+      meta: {
+        title: 'Manajemen TUK',
+        icon: 'zip',
+        noCache: true,
+        permissions: ['view menu examiner team'],
+      },
+    },
+    {
+      path: 'create',
+      component: () => import('@/views/tuk-management/Create'),
+      name: 'createTuk',
+      hidden: true,
+      meta: {
+        title: 'Tambah TUK',
+        icon: 'documentation',
+        noCache: true,
+      },
+    },
+    {
+      path: 'edit/:id',
+      component: () => import('@/views/tuk-management/Create'),
+      name: 'editTuk',
+      hidden: true,
+      meta: { title: 'Edit TUK', icon: 'documentation', noCache: true },
+    },
+    {
+      path: ':id/anggota',
+      component: () => import('@/views/tuk-management/TeamMember'),
+      name: 'kelolaTuk',
+      hidden: true,
+      meta: { title: 'Kelola TUK', icon: 'documentation', noCache: true },
     },
   ],
 };
