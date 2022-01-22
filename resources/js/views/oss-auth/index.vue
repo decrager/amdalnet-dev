32127a877657634ae01d3e9a11c92ddb1aa48caa
Achmad Giovani <achmad.giovani@imaniprima.com>
2022-01-22 13:57:23 +0700

    remove email & username from url

--- resources/js/views/oss-auth/index.vue
+++ resources/js/views/oss-auth/index.vue
@@ -4,7 +4,6 @@
 
 <script>
 import axios from 'axios';
-// import Resource from '@/api/resource';
 import { isLogged, setLogged } from '@/utils/auth';
 
 export default {
@@ -13,8 +12,6 @@ export default {
     return {
       fullscreenLoading: false,
       to: this.$route.query.to,
-      email: this.$route.query.email,
-      username: this.$route.query.username,
       token: this.$route.query.token,
       userInfo: {},
       isTokenValid: false,
@@ -22,16 +19,29 @@ export default {
     };
   },
   mounted() {
+    // TODO: eliminate email & username from query
     this.fullscreenLoading = true;
-    this.isEmailRegistered()
+    if (this.$route.query.to === undefined || this.$route.query.token === undefined) {
+      this.fullscreenLoading = false;
+      this.$message({
+        message: 'Link tidak valid. Silakan coba lagi.',
+        type: 'error',
+        duration: 5 * 1000,
+      });
+      this.$router.push({ path: '/' });
+    }
+    this.getUserInfo()
       .then(response => {
-        this.validateToken()
+        this.isEmailRegistered()
           .then(response => {
-            // console.log('this.isUserExists = ' + this.isUserExists);
-            // console.log('this.isTokenValid = ' + this.isTokenValid);
-            this.redirect()
+            this.validateToken()
               .then(response => {
-                this.fullscreenLoading = false;
+                console.log('this.isUserExists = ' + this.isUserExists);
+                console.log('this.isTokenValid = ' + this.isTokenValid);
+                this.redirect()
+                  .then(response => {
+                    this.fullscreenLoading = false;
+                  });
               });
           });
       });
@@ -58,37 +68,34 @@ export default {
         });
         this.$router.push({ path: '/' });
       } else if (!this.isUserExists) {
-        this.getUserInfo()
-          .then(response => {
-            // Register
-            this.$router.push({
-              name: 'OssAuthRegister',
-              params: {
-                userInfo: {
-                  email: this.email,
-                  username: this.username,
-                  name: this.userInfo.nama, // TODO: get detail NIB from OSShub
-                  pic: this.userInfo.nama,
-                  phone: this.userInfo.telp,
-                  address: this.userInfo.alamat,
-                  nib: this.userInfo.data_nib[0],
-                },
-              },
-            });
-          });
+        // Register
+        this.$router.push({
+          name: 'OssAuthRegister',
+          params: {
+            userInfo: {
+              email: this.userInfo.email,
+              username: this.userInfo.username,
+              name: this.userInfo.nama, // TODO: get detail NIB from OSShub
+              pic: this.userInfo.nama,
+              phone: this.userInfo.telp,
+              address: this.userInfo.alamat,
+              nib: this.userInfo.data_nib[0],
+            },
+          },
+        });
       }
     },
     loginOss() {
       // login with OSS
       axios.post('api/auth/login-oss', {
-        email: this.email,
+        email: this.userInfo.email,
         token: this.token,
       })
         .then(response => {
           if (response.status === 200) {
             setLogged('1');
             this.$message({
-              message: 'Anda berhasil login sebagai ' + this.email,
+              message: 'Anda berhasil login sebagai ' + this.userInfo.email,
               type: 'success',
               duration: 5 * 1000,
             });
@@ -108,7 +115,7 @@ export default {
     },
     async isEmailRegistered() {
       this.isUserExists = false;
-      await axios.get('api/auth/is-email-registered?email=' + this.email)
+      await axios.get('api/auth/is-email-registered?email=' + this.userInfo.email)
         .then(response => {
           if (response.status === 200) {
             this.isUserExists = true;
