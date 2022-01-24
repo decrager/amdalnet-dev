4ef74a220cc8f094dde46951d3a0b247cf35280b
Hernowo <eno.erno@gmail.com>
2022-01-25 01:19:04 +0700

    refaktor

--- resources/js/views/master-izin/index.vue
+++ resources/js/views/master-izin/index.vue
@@ -2,7 +2,6 @@
   <div class="app-container" style="padding: 24px">
     <el-card>
       <div class="filter-container">
-        <!-- <h2>Materi</h2> -->
         <el-button
           class="filter-item"
           type="primary"
@@ -12,22 +11,6 @@
           {{ 'Tambah Izin' }}
         </el-button>
       </div>
-      <!-- <el-table :data="tableData" border style="width: 100%">
-        <el-table-column prop="no" label="No" width="70" />
-        <el-table-column prop="judul" label="Judul Materi" />
-        <el-table-column prop="deskripsi" label="Deskripsi" />
-        <el-table-column prop="tanggal" label="Tanggal Terbit" />
-        <el-table-column prop="download" label="Download" width="180">
-          <template>
-            <el-button type="text" href="#" icon="el-icon-edit">
-              Ubah
-            </el-button>
-            <el-button type="text" href="#" icon="el-icon-delete">
-              Hapus
-            </el-button>
-          </template>
-        </el-table-column>
-      </el-table> -->
       <component-table
         :loading="loading"
         :list="allData"
@@ -42,13 +25,6 @@
         :limit.sync="listQuery.limit"
         @pagination="handleFilter"
       />
-      <!-- <component-dialog
-        :component="component"
-        :show="show"
-        :list-view="listView"
-        :list-view-title="listViewTitle"
-        @handleCancelComponent="handleCancelComponent"
-      /> -->
     </el-card>
   </div>
 </template>
@@ -63,7 +39,6 @@ export default {
   components: {
     Pagination,
     ComponentTable,
-    // ComponentDialog,
   },
   data() {
     return {
