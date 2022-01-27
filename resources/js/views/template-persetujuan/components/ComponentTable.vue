ff09083a027376225d2b41b6a016427d219a07c3
Hernowo <eno.erno@gmail.com>
2022-01-27 18:43:13 +0700

    layout

---
+++ resources/js/views/template-persetujuan/components/ComponentTable.vue
@@ -0,0 +1,101 @@
+<template>
+  <el-table
+    v-loading="loading"
+    :data="list"
+    fit
+    highlight-current-row
+    :header-cell-style="{ background: '#3AB06F', color: 'white', border:'0' }"
+  >
+    <el-table-column label="No." width="54px">
+      <template slot-scope="scope">
+        <span>{{ scope.$index + 1 }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="Jenis Template">
+      <template slot-scope="scope">
+        <span>{{ scope.row.pemarkasa_name }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="Deskripsi">
+      <template slot-scope="scope">
+        <span>{{ scope.row.kegiatan_name }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="Download/File">
+      <template slot-scope="scope">
+        <span>{{ scope.row.sk_number }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="Aksi">
+      <template slot-scope="scope">
+        <el-button
+          type="text"
+          href="#"
+          icon="el-icon-edit"
+          @click="handleEditForm(scope.row)"
+        >
+          Ubah
+        </el-button>
+        <el-button
+          type="text"
+          href="#"
+          icon="el-icon-delete"
+          @click="handleDelete(scope.row)"
+        >
+          Hapus
+        </el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: 'ComponentTable',
+  props: {
+    list: {
+      type: Array,
+      default: () => [],
+    },
+    loading: Boolean,
+  },
+  methods: {
+    handleEditForm(row) {
+      const currentParam = row;
+      this.$router.push({
+        name: 'EditIzin',
+        params: { row, appParams: currentParam },
+      });
+    },
+    handleDelete(rows) {
+      this.$emit('handleDelete', { rows });
+    },
+    formatDateStr(date) {
+      const today = new Date(date);
+      var bulan = [
+        'Januari',
+        'Februari',
+        'Maret',
+        'April',
+        'Mei',
+        'Juni',
+        'Juli',
+        'Agustus',
+        'September',
+        'Oktober',
+        'November',
+        'Desember',
+      ];
+      const year = today.getFullYear();
+      const month = today.getMonth();
+      const day = today.getDate();
+      const monthID = bulan[month];
+      const finalDate = `${day} ${monthID} ${year}`;
+      return finalDate;
+    },
+  },
+};
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .el-table-column{border: none;}
+</style>
---
+++ resources/js/views/template-persetujuan/index.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="app-container" style="padding: 24px">
+    <el-card>
+      <div class="filter-container">
+        <el-button
+          class="filter-item"
+          type="primary"
+          icon="el-icon-plus"
+          @click="handleCreate"
+        >
+          {{ 'Tambah Persetujuan Lingkungan' }}
+        </el-button>
+      </div>
+      <component-table
+        :loading="loading"
+        :list="allData"
+        @handleEditForm="handleEditForm($event)"
+        @handleDelete="handleDelete($event)"
+      />
+      <pagination
+        v-show="total > 0"
+        :total="total"
+        :page.sync="listQuery.page"
+        :limit.sync="listQuery.limit"
+        @pagination="handleFilter"
+      />
+    </el-card>
+  </div>
+</template>
+
+<script>
+import Pagination from '@/components/Pagination';
+import axios from 'axios';
+import ComponentTable from './components/ComponentTable.vue';
+
+export default {
+  name: 'DaftarIzins',
+  components: {
+    Pagination,
+    ComponentTable,
+  },
+  data() {
+    return {
+      loading: true,
+      allData: [],
+      total: 0,
+      listQuery: {
+        page: 1,
+        limit: 10,
+      },
+      optionValue: null,
+      sort: 'DESC',
+    };
+  },
+  created() {
+    this.getAll();
+  },
+  methods: {
+    handleFilter() {
+      this.getAll();
+    },
+    getAll(search, sort) {
+      this.loading = true;
+      axios
+        .get(
+          `/api/environmental-permit?page=${this.listQuery.page}&sort=${this.sort}`
+        )
+        .then((response) => {
+          this.allData = response.data.data;
+          this.total = response.data.total;
+          this.loading = false;
+        });
+    },
+    handleCreate() {
+      this.$router.push({
+        name: 'AddDaftarPersetujuan',
+      });
+    },
+    handleDelete({ rows }) {
+      this.$confirm('apakah anda yakin akan menghapus ' + rows.id + '. ?', 'Peringatan', {
+        confirmButtonText: 'OK',
+        cancelButtonText: 'Batal',
+        type: 'warning',
+      })
+        .then(() => {
+          axios.get(`/api/environmental-permit/delete/${rows.id}`)
+            .then((response) => {
+              this.$message({
+                type: 'success',
+                message: 'Hapus Selesai',
+              });
+              this.getAll();
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: 'Hapus Digagalkan',
+          });
+        });
+    },
+  },
+};
+</script>
+
