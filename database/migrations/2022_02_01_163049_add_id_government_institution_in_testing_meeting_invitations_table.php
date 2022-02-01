4d3939988ec6ee51b6d1f2fc840a85484a33a668
Salman Said Ramadhan <42852021+salmansaidr@users.noreply.github.com>
2022-02-01 16:34:21 +0700

    add id_government_institution in testing_meeting_invitations table

---
+++ database/migrations/2022_02_01_163049_add_id_government_institution_in_testing_meeting_invitations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIdGovernmentInstitutionInTestingMeetingInvitationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('testing_meeting_invitations', function (Blueprint $table) {
+            $table->integer('id_government_institution')->nullable();
+            $table->foreign('id_government_institution')->references('id')->on('government_institutions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('testing_meeting_invitations', function (Blueprint $table) {
+            $table->dropForeign(['id_government_institution']);
+            $table->dropColumn('id_government_institution');
+        });
+    }
+}
