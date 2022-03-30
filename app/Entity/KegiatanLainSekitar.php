8a8abe98d003dffc921c18ee7dd54d5f15f40ca5
Husnul Hasanah <husnul.hasanah@gmail.com>
2022-03-30 11:06:57 +0700

    preparing, kegiatan lain sekitar

---
+++ app/Entity/KegiatanLainSekitar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Entity;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class KegiatanLainSekitar extends Model
+{
+    use SoftDeletes;
+
+    // table kegiatan_lain_sekitars
+}
