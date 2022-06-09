c9df6ca442b2ccb1c2a0000cebce46326b50da30
Salman Said Ramadhan <42852021+salmansaidr@users.noreply.github.com>
2022-06-09 13:20:11 +0700

    add attachment table

---
+++ resources/js/views/review-dokumen/Lampiran.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-table
+    :data="list"
+    fit
+    highlight-current-row
+    :header-cell-style="{ background: '#3AB06F', color: 'white' }"
+    style="margin-top: 15px"
+  >
+    <el-table-column align="center" label="No" width="50px">
+      <template slot-scope="scope">
+        <span>{{ scope.$index + 1 }}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column align="center" label="Lampiran">
+      <template slot-scope="scope">
+        <span>{{ scope.row.name }}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column align="center" label="File">
+      <template slot-scope="scope">
+        <a :href="scope.row.file" download>Unduh File PDF</a>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: 'Lampiran',
+  data() {
+    return {
+      list: [],
+    };
+  },
+};
+</script>
