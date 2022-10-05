4fa56b55fb1eceb4ac3f9ab6ea56882e34e45aa6
setiyo gunawan <setiyo@brainmatics.com>
2022-10-05 14:00:13 +0700

    [FIX] Fix typo

--- resources/js/views/amdal/components/UploadPetaBatas.vue
+++ resources/js/views/amdal/components/UploadPetaBatas.vue
@@ -12,7 +12,7 @@
         <el-col :span="11" style="margin-right:1em;">
 
           <fieldset style="border:1px solid #e0e0e0; border-radius: 0.3em; width:100%; padding: .5em;">
-            <legend style="margin:0 2em;">File-File SHP yang suda di-zip <small style="color: red;">(Maks. 10 MB)</small>
+            <legend style="margin:0 2em;">File-File SHP yang sudah di-zip <small style="color: red;">(Maks. 10 MB)</small>
               <div v-if="petaEkologisSHP != ''" class="current">tersimpan: <span style="color: green" @click="download(idPES)"><strong>{{ petaEkologisSHP }}<i class="el-icon-circle-check" /></strong></span>
                 <!-- &nbsp;<i class="el-icon-delete"></i>-->
               </div>
@@ -40,7 +40,7 @@
       <el-form-item label="Peta Batas Sosial" :required="required">
         <el-col :span="11" style="margin-right:1em;">
           <fieldset style="border:1px solid #e0e0e0; border-radius: 0.3em; width:100%; padding: .5em;">
-            <legend style="margin:0 2em;">File-File SHP yang suda di-zip <small style="color: red;">(Maks. 10 MB)</small>
+            <legend style="margin:0 2em;">File-File SHP yang sudah di-zip <small style="color: red;">(Maks. 10 MB)</small>
               <div v-if="petaSosialSHP != ''" class="current">tersimpan: <span style="color: green" @click="download(idPSS)"><strong>{{ petaSosialSHP }}<i class="el-icon-circle-check" /></strong></span></div>
             </legend>
 
@@ -69,7 +69,7 @@
       <el-form-item label="Peta Batas Wilayah Studi" :required="required">
         <el-col :span="11" style="margin-right:1em;">
           <fieldset style="border:1px solid #e0e0e0; border-radius: 0.3em; width:100%; padding: .5em;">
-            <legend style="margin:0 2em;">File-File SHP yang suda di-zip <small style="color: red;">(Maks. 10 MB)</small>
+            <legend style="margin:0 2em;">File-File SHP yang sudah di-zip <small style="color: red;">(Maks. 10 MB)</small>
               <div v-if="petaStudiSHP != ''" class="current">tersimpan: <span style="color: green" @click="download(idPSuS)"><strong>{{ petaStudiSHP }}<i class="el-icon-circle-check" /></strong></span></div>
             </legend>
 
