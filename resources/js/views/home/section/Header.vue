76d41a1d0ebff3bb4b8af4051805ca538d9b1c3a
Achmad Fatkharrofiqi <achmadfatkharrofiqi404@gmail.com>
2022-10-21 14:41:02 +0700

    [ADD] add page info persuratan

--- resources/js/views/home/section/Header.vue
+++ resources/js/views/home/section/Header.vue
@@ -70,6 +70,9 @@
               <li>
                 <a href="#" @click="handleSetMenu('LPJP')">Daftar LPJP</a>
               </li>
+              <li>
+                <a href="/info_persuratan">Informasi Persuratan</a>
+              </li>
             </ul>
           </li>
           <li class="nav__item">
