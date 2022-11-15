176766362c468cdad221c844cf2850fcf713fe9c
setiyo gunawan <setiyo@brainmatics.com>
2022-11-15 09:11:06 +0700

    [ADD] Add rekap workspace

--- resources/js/views/workspace/Office.vue
+++ resources/js/views/workspace/Office.vue
@@ -1,16 +1,221 @@
 <template>
   <div class="app-container">
-    <div id="placeholder" />
+    <div id="etherpad-wrapper" style="height: 80%;">
+      <div id="placeholder" />
+    </div>
+    <div class="uji-collab" style="margin-bottom: 10vh;">
+      <div style="width: 100%; margin: 1vh;">
+        <h2>Saran & Masukan</h2>
+      </div>
+      <div style="display: flex; flex-direction: row; margin-bottom: 1vh; justify-content: space-between;">
+        <el-select
+          v-model="idCat"
+          placeholder="Pilih Kategori"
+        >
+          <el-option
+            v-for="item in categories"
+            :key="item.id"
+            :label="item.title"
+            :value="item.id"
+          />
+        </el-select>
+        <el-button>Simpan</el-button>
+      </div>
+      <div style="display: flex;">
+        <el-collapse v-model="activeNames" accordion style="width: 100%;">
+          <el-collapse-item :title="this.$store.getters.user.name">
+            <el-button v-if="rekaps.length === 0" @click="addNewRekap">Tambah</el-button>
+            <div
+              v-for="(rekap, index) in rekaps"
+              :key="rekap.id"
+              style="display: flex;"
+            >
+              <div>
+                <span>Saran/ Pendapat/ Tanggapan</span>
+                <TextEditor
+                  v-model="rekaps[index].saran_pendapat_tanggapan"
+                  output-format="html"
+                  :menubar="''"
+                  :image="false"
+                  :toolbar="['bold italic underline bullist numlist fullscreen']"
+                  :height="50"
+                />
+              </div>
+              <div>
+                <span>Halaman</span>
+                <el-input v-model="rekaps[index].halaman_tuk" />
+              </div>
+              <div>
+                <span>Perbaikan/ Tanggapan Pemrakarsa</span>
+                <TextEditor
+                  v-model="rekaps[index].perbaikan"
+                  output-format="html"
+                  :menubar="''"
+                  :image="false"
+                  :toolbar="['bold italic underline bullist numlist fullscreen']"
+                  :height="50"
+                />
+              </div>
+              <div>
+                <span>Halaman</span>
+                <el-input v-model="rekaps[index].halaman_perbaikan" />
+              </div>
+              <div style="display: flex; flex-direction: column-reverse;">
+                <el-button @click="addNewRekap">Tambah</el-button>
+                <el-button @click="rekaps.splice(index, 1)">Hapus</el-button>
+              </div>
+              <!-- <el-button
+                @click="rekaps.splice(index, 1)"
+              >
+                IKI endi tombol e
+              </el-button> -->
+            </div>
+          </el-collapse-item>
+        </el-collapse>
+        <!-- <el-button @click="addNewRekap">Tambah</el-button> -->
+      </div>
+      <!-- <div>
+        <div style="display: flex;">
+          <el-collapse v-model="activeNames" accordion style="width: 90%;">
+            <el-collapse-item name="5" title="">
+              <template slot="title">
+                <el-input v-model="input" class="test" placeholder="Please input" style="width: 50%;" />
+              </template>
+              <div style="display: flex;">
+                <div>
+                  <span>Saran/ Pendapat/ Tanggapan</span>
+                  <TextEditor
+                    v-model="comment"
+                    output-format="html"
+                    :menubar="''"
+                    :image="false"
+                    :toolbar="['bold italic underline bullist numlist fullscreen']"
+                    :height="50"
+                  />
+                </div>
+                <div>
+                  <span>Halaman</span>
+                  <el-input />
+                </div>
+                <div>
+                  <span>Perbaikan/ Tanggapan Pemrakarsa</span>
+                  <TextEditor
+                    v-model="comment"
+                    output-format="html"
+                    :menubar="''"
+                    :image="false"
+                    :toolbar="['bold italic underline bullist numlist fullscreen']"
+                    :height="50"
+                  />
+                </div>
+                <div>
+                  <span>Halaman</span>
+                  <el-input />
+                </div>
+                <el-button>Tambah</el-button>
+              </div>
+              <div style="display: flex;">
+                <div>
+                  <span>Saran/ Pendapat/ Tanggapan</span>
+                  <TextEditor
+                    v-model="comment"
+                    output-format="html"
+                    :menubar="''"
+                    :image="false"
+                    :toolbar="['bold italic underline bullist numlist fullscreen']"
+                    :height="50"
+                  />
+                </div>
+                <div>
+                  <span>Halaman</span>
+                  <el-input />
+                </div>
+                <div>
+                  <span>Perbaikan/ Tanggapan Pemrakarsa</span>
+                  <TextEditor
+                    v-model="comment"
+                    output-format="html"
+                    :menubar="''"
+                    :image="false"
+                    :toolbar="['bold italic underline bullist numlist fullscreen']"
+                    :height="50"
+                  />
+                </div>
+                <div>
+                  <span>Halaman</span>
+                  <el-input />
+                </div>
+              </div>
+              <div style="display: flex;">
+                <div>
+                  <span>Saran/ Pendapat/ Tanggapan</span>
+                  <TextEditor
+                    v-model="comment"
+                    output-format="html"
+                    :menubar="''"
+                    :image="false"
+                    :toolbar="['bold italic underline bullist numlist fullscreen']"
+                    :height="50"
+                  />
+                </div>
+                <div>
+                  <span>Halaman</span>
+                  <el-input />
+                </div>
+                <div>
+                  <span>Perbaikan/ Tanggapan Pemrakarsa</span>
+                  <TextEditor
+                    v-model="comment"
+                    output-format="html"
+                    :menubar="''"
+                    :image="false"
+                    :toolbar="['bold italic underline bullist numlist fullscreen']"
+                    :height="50"
+                  />
+                </div>
+                <div>
+                  <span>Halaman</span>
+                  <el-input />
+                </div>
+              </div>
+            </el-collapse-item>
+          </el-collapse>
+          <el-button>Tambah</el-button>
+        </div>
+        <el-collapse v-model="activeNames" accordion style="width: 80%;">
+          <el-collapse-item name="3" title="">
+            <template slot="title">
+              <el-input v-model="input" class="test" placeholder="Please input" style="width: 50%;" />
+            </template>
+            <div>
+              <strong>
+                Consistent with real life: in line with the process and logic of real
+                life, and comply with languages and habits that the users are used to;
+              </strong>
+            </div>
+          </el-collapse-item>
+          <el-collapse-item name="7" title="Bab 2">
+            <div>
+              <strong>
+                Consistent with real life: in line with the process and logic of real
+                life, and comply with languages and habits that the users are used to;
+              </strong>
+            </div>
+          </el-collapse-item>
+        </el-collapse>
+      </div> -->
+    </div>
   </div>
 </template>
 
 <script>
 import WorkspaceResource from '@/api/workspace';
-
+import TextEditor from '@/components/Tinymce';
 const workspaceResource = new WorkspaceResource();
 
 export default {
   components: {
+    TextEditor,
   },
   props: {
     project: {
@@ -24,11 +229,37 @@ export default {
   },
   data() {
     return {
+      input: '',
+      comment: null,
+      activeNames: '',
       officeUrl: '',
       selectedTreeId: 1,
       sessionID: null,
       loading: false,
       docEditor: null,
+      idCat: null,
+      categories: [
+        {
+          id: 1,
+          title: 'Pakar Ahli',
+        },
+        {
+          id: 2,
+          title: 'Instansi Pusat',
+        },
+        {
+          id: 3,
+          title: 'Instansi Daerah',
+        },
+      ],
+      rekaps: [
+        {
+          saran_pendapat_tanggapan: 'Saran pendapat tanggapan',
+          halaman_tuk: 5,
+          perbaikan: 'Perbaikan',
+          halaman_perbaikan: 7,
+        },
+      ],
     };
   },
   computed: {
@@ -40,6 +271,13 @@ export default {
       return '';
     },
   },
+  watch: {
+    activeNames: function(val) {
+      console.log({ activeName: val });
+      console.log({ data: this.$store.getters });
+      this.activeName = val[1];
+    },
+  },
   async mounted() {
     console.log('props:', this.$route.params.id, this.project, process.env.MIX_BASE_API);
     // console.log(process.env.MIX_OFFICE_URL);
@@ -47,6 +285,14 @@ export default {
     this.addOfficeScript();
   },
   methods: {
+    addNewRekap: function() {
+      this.rekaps.push({
+        saran_pendapat_tanggapan: null,
+        halaman_tuk: null,
+        perbaikan: null,
+        halaman_perbaikan: null,
+      });
+    },
     resize() {
       console.log('resize');
     },
@@ -126,6 +372,9 @@ export default {
 </script>
 
 <style lang="scss">
+  body {
+    overflow: hidden;
+  }
   .vtl {
     .vtl-tree-margin {
       margin-left: 1em;
@@ -156,6 +405,7 @@ export default {
   }
   .app-container {
     height: 100vh;
+    overflow: scroll;
   }
 
   .left-container {
