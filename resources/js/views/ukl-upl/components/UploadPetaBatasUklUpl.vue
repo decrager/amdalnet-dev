8326e1aa13f0238d41522764a604379582f555ed
haridsyah1 <haridsyah@brainmatics.com>
2022-12-22 09:42:43 +0700

    [FIX] Upload peta ZIP MacOS

--- resources/js/views/ukl-upl/components/UploadPetaBatasUklUpl.vue
+++ resources/js/views/ukl-upl/components/UploadPetaBatasUklUpl.vue
@@ -1025,7 +1025,7 @@ export default {
       switch (idx) {
         case 1:
           if (this.$refs.refPengelolaanSHP.files[0].size <= 10485760) {
-            if (this.$refs.refPengelolaanSHP.files[0].type.includes('zip')) {
+            if (!this.$refs.refPengelolaanSHP.files[0].type.includes('zip')) {
               this.$refs.refPengelolaanSHP.value = null;
               this.$alert('File yang diterima hanya .zip', 'Format Peta Titik Pengelolaan Salah', {
                 center: true,
@@ -1073,7 +1073,7 @@ export default {
           break;
         case 3:
           if (this.$refs.refPemantauanSHP.files[0].size <= 10485760) {
-            if (this.$refs.refPemantauanSHP.files[0].type.includes('zip')) {
+            if (!this.$refs.refPemantauanSHP.files[0].type.includes('zip')) {
               this.$refs.refPemantauanSHP.value = null;
               this.$alert('File yang diterima hanya .zip', 'Format Peta Titik Pemantauan Salah', {
                 center: true,
@@ -1118,7 +1118,7 @@ export default {
           break;
         case 5:
           if (this.$refs.refAreaPengelolaanSHP.files[0].size <= 10485760) {
-            if (this.$refs.refAreaPengelolaanSHP.files[0].type.includes('zip')) {
+            if (!this.$refs.refAreaPengelolaanSHP.files[0].type.includes('zip')) {
               this.$refs.refAreaPengelolaanSHP.value = null;
               this.$alert('File yang diterima hanya .zip', 'Format Peta Lokasi Pengelolaan Salah', {
                 center: true,
@@ -1163,7 +1163,7 @@ export default {
           break;
         case 7:
           if (this.$refs.refAreaPemantauanSHP.files[0].size <= 10485760) {
-            if (this.$refs.refAreaPemantauanSHP.files[0].type.includes('zip')) {
+            if (!this.$refs.refAreaPemantauanSHP.files[0].type.includes('zip')) {
               this.$refs.refAreaPemantauanSHP.value = null;
               this.$alert('File yang diterima hanya .zip', 'Format Peta Lokasi Pemantauan Salah', {
                 center: true,
