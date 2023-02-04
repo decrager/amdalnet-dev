dc41ad6662c126eed3b0ff0e350d223fccd7f389
haridsyah1 <haridsyah@brainmatics.com>
2023-02-04 16:17:13 +0700

    [CHANGE] Disclaimer

--- resources/js/views/home/components/Disclaimer.vue
+++ resources/js/views/home/components/Disclaimer.vue
@@ -8,12 +8,12 @@
     :destroy-on-close="true"
     width="45%"
   >
-    <p class="first-par"><strong>Amdalnet</strong> sudah dapat digunakan untuk pengajuan persetujuan lingkungan rencana kegiatan pemerintah dalam rangka mendukung pelaksanaan
-      <strong>UU no 11 Tahun 2020 tentang Cipta Kerja</strong> dan <strong>PP Nomor 22 Tahun 2021 tentang Penyelenggaraan Perlindungan dan Pengelolaan Lingkungan Hidup</strong>.</p>
-    <p>Khusus untuk rencana kegiatan pelaku usaha, saat ini masih dalam tahap integrasi dengan OSS-RBA.</p>
+
+    <p class="first-par">Dalam rangka mendukung pelaksanaan UU Nomor 11 Tahun 2020 tentang Cipta Kerja dan PP Nomor 22 Tahun 2021 tentang Penyelenggaraan Perlindungan dan Pengelolaan Lingkungan Hidup, Amdalnet sudah dapat digunakan untuk pengajuan persetujuan lingkungan mulai dari Penapisan Otomatis Jenis Dokumen Lingkungan, Asistensi Pelingkupan, dan Amdal Digital Workspace.</p>
+    <p>Integrasi Amdal dengan OSS-RBA telah dilakukan untuk rencana usaha dan/atau kegiatan tingkat resiko Menengah Rendah dan Rendah, sedangkan bagi rencana usaha dan/atau kegiatan baru dengan pendekatan tunggal/terpadu untuk tingkat resiko Menengah Tinggi dan Tinggi dapat diproses dengan registrasi secara mandiri melalui Amdalnet setelah mendapatkan NIB dari OSS.</p>
     <p>Terima kasih.</p>
     <!--
-    <p>Dalam rangka mendukung pelaksanaan <strong>UU No 11 Tahun 2020 tentang Cipta Kerja</strong> dan <strong>PP Nomor 22 Tahun 2021 tentang Penyelenggaraan Perlindungan dan Pengelolaan Lingkungan Hidup</strong>,
+    <p>Dalam rangka mendukung pelaksanaanUU No 11 Tahun 2020 tentang Cipta Kerja</strong> dan <strong>PP Nomor 22 Tahun 2021 tentang Penyelenggaraan Perlindungan dan Pengelolaan Lingkungan Hidup</strong>,
     maka saat ini aplikasi <strong>Amdalnet</strong> sedang dalam <span style="text-decoration: underline;">tahap pengembangan</span>.</p>
     <p>Mohon maaf atas ketidaknyamanannya. Terima kasih.</p>
     -->
