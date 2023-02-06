1a2718b7de9077d92971c624bcf1ac7530150e0d
haridsyah1 <haridsyah@brainmatics.com>
2023-02-06 10:18:26 +0700

    [FIX] Header Dashboard MR

--- resources/js/views/dashboard/admin/components/Header.vue
+++ resources/js/views/dashboard/admin/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <el-button style="font-weight: bold;" :loading="loading" type="warning" @click="showHide">Daftar Rekap SPPL dan UKL-UPL Menengah Rendah</el-button>
-    <div v-if="showIframe" style="position: absolute; background-color: #404040; left: 0; right: 0; padding-top: 1rem; padding-right: 1rem; padding-left: 1rem; margin-left: 1px; height: 100%;">
+    <div v-if="showIframe" style="position: absolute; background-color: rgb(238 238 229); z-index: 100; left: 0; right: 0; padding-top: 1rem; padding-right: 1rem; padding-left: 1rem; margin-left: 1px; height: 100%;">
       <iframe
         :src="iframeSrc"
         width="100%"
-        height="600px"
+        height="100%"
         frameborder="0"
       />
     </div>
@@ -17,8 +17,15 @@ export default {
   data() {
     return {
       showIframe: false,
-      iframeSrc: 'https://amdalnet-dev.menlhk.go.id/dashboard_mr',
+      iframeSrc: '',
     };
+  },
+  created() {
+    if (window.location.href.includes('amdalnet-dev')) {
+      this.iframeSrc = 'https://amdalnet-dev.menlhk.go.id/dashboard_mr';
+    } else {
+      this.iframeSrc = 'https://amdalnet.menlhk.go.id/dashboard_mr';
+    }
   },
   methods: {
     showHide() {
