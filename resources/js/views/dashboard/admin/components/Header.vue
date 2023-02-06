09d51ed4b6e0058fe96d711aabaa025713aef45c
haridsyah1 <haridsyah@brainmatics.com>
2023-02-06 10:43:56 +0700

    [FIX] Header

--- resources/js/views/dashboard/admin/components/Header.vue
+++ resources/js/views/dashboard/admin/components/Header.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
+    <div id="spinner">
+      <div>
+        <img src="http://www.ajaxload.info/images/exemples/25.gif">
+      </div>
+    </div>
     <el-button style="font-weight: bold;" :loading="loading" type="warning" @click="showHide">Daftar Rekap SPPL dan UKL-UPL Menengah Rendah</el-button>
     <div v-if="showIframe" style="position: absolute; background-color: rgb(238 238 229); z-index: 100; left: 0; right: 0; padding-top: 1rem; padding-right: 1rem; padding-left: 1rem; margin-left: 1px; height: 100%;">
       <iframe
         :src="iframeSrc"
         class="iframe-placeholder"
+        onload="document.getElementById('spinner').style.display='none';"
         width="100%"
         height="100%"
         frameborder="0"
@@ -38,9 +44,4 @@ export default {
 </script>
 
 <style scoped>
-.iframe-placeholder
-{
-  background-image: url("/Includes/Site/images/AjaxLoader.gif");
-  background-repeat: no-repeat;
-}
 </style>
